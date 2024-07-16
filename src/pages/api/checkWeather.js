cb3991055d08776a1e113885fb66c2411903ce05
2027-hy <cshy22027@g.nihon-u.ac.jp>
2024-07-16 18:05:22 +0900

    coment

--- src/pages/api/checkWeather.js
+++ src/pages/api/checkWeather.js
@@ -39,28 +39,44 @@ const getWeatherForecast = async (city) => {
   }
 };
 
+const sendSlackNotification = async (message) => {
+  const payload = { text: message };
+
+  try {
+    const response = await axios.post(process.env.SLACK_WEBHOOK_URL, payload);
+    if (response.status !== 200) {
+      throw new Error(`Request to Slack returned an error ${response.status}, the response is:\n${response.data}`);
+    }
+    console.log('Notification sent successfully');
+  } catch (error) {
+    console.error('Error sending Slack notification:', error.message);
+  }
+};
+
+const checkWeatherAndNotify = async () => {
+  const rainProbabilityKawaguchi = await getWeatherForecast(cities.kawaguchi);
+  const rainProbabilityFunabashi = await getWeatherForecast(cities.funabashi);
+
+  if (rainProbabilityKawaguchi !== null && rainProbabilityFunabashi !== null) {
+    if (rainProbabilityKawaguchi >= 50 && rainProbabilityFunabashi >= 50) {
+      await sendSlackNotification(`川口市、船橋市の両方とも降水確率は50%以上です。傘を持ちましょう。\n
+        川口市：${rainProbabilityKawaguchi.toFixed(2)}%、船橋市：${rainProbabilityFunabashi.toFixed(2)}%`);
+    } else if (rainProbabilityKawaguchi >= 50) {
+      await sendSlackNotification(`川口市は降水確率は${rainProbabilityKawaguchi.toFixed(2)}%です。傘を持ちましょう。`);
+    } else if (rainProbabilityFunabashi >= 50) {
+      await sendSlackNotification(`船橋市は降水確率は${rainProbabilityFunabashi.toFixed(2)}%です。傘を持ちましょう。`);
+    } else {
+      await sendSlackNotification('今日の降水確率は50%未満です。傘は必要ありません。');
+    }
+  } else {
+    await sendSlackNotification('天気予報の取得に失敗しました。');
+  }
+};
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const rainProbabilityKawaguchi = await getWeatherForecast(cities.kawaguchi);
-    const rainProbabilityFunabashi = await getWeatherForecast(cities.funabashi);
-
-    if (rainProbabilityKawaguchi !== null && rainProbabilityFunabashi !== null) {
-      let message = '';
-
-      if (rainProbabilityKawaguchi >= 50 && rainProbabilityFunabashi >= 50) {
-        message = `川口市、船橋市の両方とも降水確率は50%以上です。傘を持ちましょう。\n川口市：${rainProbabilityKawaguchi.toFixed(2)}%、船橋市：${rainProbabilityFunabashi.toFixed(2)}%`;
-      } else if (rainProbabilityKawaguchi >= 50) {
-        message = `川口市は降水確率は${rainProbabilityKawaguchi.toFixed(2)}%です。傘を持ちましょう。`;
-      } else if (rainProbabilityFunabashi >= 50) {
-        message = `船橋市は降水確率は${rainProbabilityFunabashi.toFixed(2)}%です。傘を持ちましょう。`;
-      } else {
-        message = "今日の降水確率は50%未満です。傘は必要ありません。";
-      }
-
-      res.status(200).json({ message });
-    } else {
-      res.status(500).json({ message: '天気予報の取得に失敗しました。' });
-    }
+    await checkWeatherAndNotify();
+    res.status(200).json({ message: '通知が送信されました。' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }
